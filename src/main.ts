056c9404aa1e53a92c537a805f537cd9069dd065
guanxiaomin <327547931@qq.com>
2021-10-15 14:13:34 +0800

    update packagejons

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { GRoot, UIPackage, Handler, GButton, GComponent, GList, GObject, InteractiveEvent } from "fairygui-phaser";
-import "tooqingphaser";
+import "phaser";
 // import dat from "./dat.gui";
 class MyScene extends Phaser.Scene {
     private _view: GComponent;
