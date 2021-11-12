397883416d48d395c44246a53bdca08b4dd064fe
guanxiaomin <327547931@qq.com>
2021-11-12 16:25:04 +0800

    add Button click fun

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { MovieClipScene } from './MovieClip/MovieClipScene';
-import "phaser3";
+import "phaser";
 import { ImageScene } from "./Image/ImageScene";
 import { BagPanelScene } from "./TooqingUI/BagPanel/BagPanelScene";
 import { TreeNodeScene } from "./TreeNode/TreeNodeScene";
 import { PullToRefreshScene } from './PullToRefresh/PullToRefreshScene';
 import { BasicsScene } from './Basics/BasicsScene';
+import { ButtonScene } from './Button/ButtonScene';
 
 var config = {
     type: Phaser.AUTO,
@@ -23,4 +24,4 @@ var config = {
 var game = new Phaser.Game(config);
 
 // 切换不同的scene演示不同的ui组件 
-game.scene.add('uiScene', BasicsScene, true, { x: 0, y: 0 });
+game.scene.add('uiScene', ButtonScene, true, { x: 0, y: 0 });
