0a34ac98945f60ff75802e4a2b4381053bff29d2
guanxiaomin <327547931@qq.com>
2021-11-15 15:57:12 +0800

    update ui init code

--- src/Scroll/ScrollScene.ts
+++ src/Scroll/ScrollScene.ts
@@ -13,10 +13,16 @@ export class ScrollScene extends Phaser.Scene {
     }
 
     create(data) {
-        // 初始化ui
+        const width = 2000;
+        const height = 2000;
+        const con = this.add.container(0, 0);
+        con.setSize(width, height);
+        con.setInteractive();
+        // 初始化ui,为了不影响外部ui的逻辑，直接将container传入ui库中，不影响
         GRoot.inst.attachTo(this, {
             osd: "", res: "assets/",
-            resUI: "assets/", dpr: 1, designWidth: 2000, designHeight: 2000
+            resUI: "assets/", dpr: 1, width, height,
+            container: con
         });
         UIPackage.loadPackage("scrollPane").then((pkg) => {
             // tslint:disable-next-line:no-console
